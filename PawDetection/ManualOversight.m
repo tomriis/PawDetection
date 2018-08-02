598912ed041d99ea3b83a77e51d29a9156f5c221
Tom Riis <tomriis11@gmail.com>
2018-08-01 19:10:15 -0600

    manual oversight uses arrow keys

--- PawDetection/ManualOversight.m
+++ PawDetection/ManualOversight.m
@@ -1,24 +1,23 @@
 function ManualOversight(Images, pawCenters,imNum)
     h = figure;
-    h.UserData = imNum;
-    set(h,'KeyPressFcn',{@KeyPressCb, Images, pawCenters, imNum});
+    h.UserData(end+1) = imNum;
+    set(h,'KeyPressFcn',{@KeyPressCb, Images, pawCenters});
     
     %disp(num2str(pawPoints{1}(1)));
     disp(num2str(h.UserData));
 end
 
-function imNum = KeyPressCb(src,evnt,Images, pawCenters, imNum)
+function KeyPressCb(hObject,evnt,Images, pawCenters)
     fprintf('key pressed: %s\n',evnt.Key);
-    h.UserData = imNum;
     if strcmp(evnt.Key,'rightarrow')==1
         s = evnt.Key;
-        h.UserData = h.UserData + 1;
+        hObject.UserData(end+1) = hObject.UserData(end) + 1;
     elseif strcmp(evnt.Key, 'leftarrow')==1
         s = evnt.Key;
-        imNum = imNum + 1;
+        hObject.UserData(end+1) = hObject.UserData(end) - 1;
     elseif strcmp(evnt.Key,'space')==1
         s = evnt.Key;    
     end
-    disp(num2str(h.UserData))
-    pawPoints = ShowPawPlacement(Images, pawCenters, h.UserData);
+    disp(num2str(hObject.UserData(end)))
+    pawPoints = ShowPawPlacement(Images, pawCenters, hObject.UserData(end));
 end
