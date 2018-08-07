159ff1cfdcc521ef873eba81a948431fc49f4519
Tom Riis <tomriis@toms-mbp.uconnect.utah.edu>
2018-08-07 10:33:26 -0600

    added coments to crop.m

--- PawDetection/Crop.m
+++ PawDetection/Crop.m
@@ -1,6 +1,7 @@
 function [ cropped ] = Crop( image,wantedRows,wantedColumns )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
+% New change here
 
 cropped = uint8(zeros(wantedRows(2) - wantedRows(1) + 1,wantedColumns(2) - wantedColumns(1) + 1,3));
 
